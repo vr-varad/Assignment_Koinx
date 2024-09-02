b1eaeb150a624478f548fd9398a427ba2377cdfc
vr-varad <varadgupta21#gmail.com>
2024-09-02 10:28:09 +0530

    feat: Configured Environment Setup

---
+++ src/config/config.ts
@@ -0,0 +1,11 @@
+import dotenvFlow from 'dotenv-flow'
+
+dotenvFlow.config()
+
+export default {
+    ENV: process.env.ENV,
+    PORT: process.env.PORT,
+    SERVER_URL: process.env.SERVER_URL,
+    ETHERSCAN_API: process.env.ETHERSCAN_API,
+    DATABASE_URL: process.env.DATABASE_URL
+}
